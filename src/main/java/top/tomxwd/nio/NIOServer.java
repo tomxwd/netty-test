305e6ed6fd1ca8d8161bce08f37d7bcdcac03ff8
tomxwd <648913416@qq.com>
2019-12-01 10:19:43 +0800

    Netty NIO实例服务器端代码

--- src/main/java/top/tomxwd/nio/NIOServer.java
+++ src/main/java/top/tomxwd/nio/NIOServer.java
@@ -25,9 +25,9 @@ public class NIOServer {
         // 把ServerSocketChannel注册到Selector，关心的事件是OP_ACCEPT
         serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
         // 循环等待客户端连接
-        while (true){
+        while (true) {
             // 这里等待一秒，如果没有事件发生就返回
-            if(selector.select(1000)==0){
+            if (selector.select(1000) == 0) {
                 // 没有事件发生
                 System.out.println("服务器等待了一秒，无连接");
                 continue;
@@ -39,25 +39,28 @@ public class NIOServer {
             Set<SelectionKey> selectionKeys = selector.selectedKeys();
             // 遍历Set<SelectionKey>集合，使用迭代器遍历
             Iterator<SelectionKey> keyIterator = selectionKeys.iterator();
-            while (keyIterator.hasNext()){
+            while (keyIterator.hasNext()) {
                 // 获取到SelectionKey
                 SelectionKey key = keyIterator.next();
                 // 根据key对应的通道发生的事件做相应的处理
-                if(key.isAcceptable()){
+                if (key.isAcceptable()) {
                     // 如果是OP_ACCEPT事件，有新的客户端连接
                     // 给该客户端生成SocketChannel
                     SocketChannel socketChannel = serverSocketChannel.accept();
+                    socketChannel.configureBlocking(false);
+                    System.out.println("客户端连接成功，生成了一个SocketChannel" + socketChannel.hashCode());
                     // 将当前的socketChannel注册到Selector上，关注事件为OP_READ，同时给SocketChannel关联一个Buffer
-                    socketChannel.register(selector,SelectionKey.OP_READ, ByteBuffer.allocate(1024));
+                    socketChannel.register(selector, SelectionKey.OP_READ, ByteBuffer.allocate(1024));
                 }
-                if(key.isReadable()){
+                if (key.isReadable()) {
                     // 如果是OP_READ事件
                     // 通过key反向获取到对应的channel
                     SocketChannel channel = (SocketChannel) key.channel();
                     // 获取到该channel关联的Buffer
                     ByteBuffer buffer = (ByteBuffer) key.attachment();
                     channel.read(buffer);
-                    System.out.println("from 客户端 : "+new String(buffer.array()));
+                    System.out.println("from 客户端 : " + new String(buffer.array()));
+                    buffer.clear();
                 }
                 // 手动从集合中删除当前的selectKey，防止重复操作
                 keyIterator.remove();
