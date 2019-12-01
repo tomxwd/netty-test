49fbc5956a86e9c95a18bd2ece63c36c29967d2f
tomxwd <648913416@qq.com>
2019-12-01 20:07:39 +0800

    Netty NIO实例客户端代码

--- src/main/java/top/tomxwd/nio/groupchat/GroupChatServer.java
+++ src/main/java/top/tomxwd/nio/groupchat/GroupChatServer.java
@@ -39,30 +39,30 @@ public class GroupChatServer {
     }
 
     // 监听
-    public void listen(){
-        try{
+    public void listen() {
+        try {
             // 循环处理
-            while (true){
+            while (true) {
                 int count = selector.select(2000);
                 // 有事件需要处理
-                if (count>0) {
+                if (count > 0) {
                     // 遍历得到的SelectionKey集合
                     Set<SelectionKey> selectionKeys = selector.selectedKeys();
                     Iterator<SelectionKey> keyIterator = selectionKeys.iterator();
-                    while (keyIterator.hasNext()){
+                    while (keyIterator.hasNext()) {
                         SelectionKey key = keyIterator.next();
                         // 监听到的是ACCPET事件
-                        if(key.isAcceptable()){
+                        if (key.isAcceptable()) {
                             SocketChannel socketChannel = listenChannle.accept();
                             // 设置为非阻塞模式
                             socketChannel.configureBlocking(false);
                             // 将该通道注册到selector中
-                            socketChannel.register(selector,SelectionKey.OP_READ, ByteBuffer.allocate(1024));
+                            socketChannel.register(selector, SelectionKey.OP_READ, ByteBuffer.allocate(1024));
                             // 给出提示XXX上线
                             System.out.println(socketChannel.getRemoteAddress() + "上线");
                         }
                         // 监听到的是READ事件
-                        if(key.isReadable()){
+                        if (key.isReadable()) {
                             // 处理读事件的方法
                             readData(key);
                         }
@@ -73,7 +73,7 @@ public class GroupChatServer {
                     System.out.println("等待");
                 }
             }
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         } finally {
 
@@ -82,29 +82,30 @@ public class GroupChatServer {
 
     /**
      * 读取客户端消息
+     *
      * @param selectionKey
      */
-    private void readData(SelectionKey selectionKey){
+    private void readData(SelectionKey selectionKey) {
         // 定义一个SocketChannel
         SocketChannel channel = null;
-        try{
+        try {
             // 取得关联的Channel
             channel = (SocketChannel) selectionKey.channel();
             // 创建Buffer
             ByteBuffer buffer = ByteBuffer.allocate(1024);
             int read = channel.read(buffer);
             // 根据read的值做处理
-            if(read>0){
+            if (read > 0) {
                 // 把Buffer转String
                 String msg = new String(buffer.array());
                 // 输出该消息
-                System.out.println("from 客户端："+msg);
+                System.out.println("from 客户端：" + msg);
                 // 向其他客户端转发消息(排除自己)
-                sendInfoToOtherClients(msg,channel);
+                sendInfoToOtherClients(msg, channel);
             }
-        } catch (IOException e){
+        } catch (IOException e) {
             try {
-                System.out.println(channel.getRemoteAddress()+" 离线了...");
+                System.out.println(channel.getRemoteAddress() + " 离线了...");
                 // 取消注册
                 selectionKey.cancel();
                 // 关闭通道
@@ -118,17 +119,18 @@ public class GroupChatServer {
 
     /**
      * 转发消息给其他客户（通过通道）
+     *
      * @param msg
      * @param self
      */
-    private void sendInfoToOtherClients(String msg,SocketChannel self) throws IOException {
+    private void sendInfoToOtherClients(String msg, SocketChannel self) throws IOException {
         System.out.println("服务器转发消息中...");
         // 遍历 所有注册到Selector上的SocketChannel并排除self
         for (SelectionKey key : selector.keys()) {
             // 通过key取出对应的SocketChannel
             Channel targetChannel = key.channel();
             // 排除self
-            if(targetChannel instanceof SocketChannel && targetChannel != self){
+            if (targetChannel instanceof SocketChannel && targetChannel != self) {
                 // 转型为SocketChannel
                 SocketChannel dest = (SocketChannel) targetChannel;
                 // 将msg存储到buffer中
