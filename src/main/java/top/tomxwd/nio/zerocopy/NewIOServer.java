cc6f8fd99948c6566a0d6c376409c1283d580921
tomxwd <648913416@qq.com>
2019-12-02 20:52:36 +0800

    transforTo IO拷贝

--- src/main/java/top/tomxwd/nio/zerocopy/NewIOServer.java
+++ src/main/java/top/tomxwd/nio/zerocopy/NewIOServer.java
@@ -1,13 +1,35 @@
 package top.tomxwd.nio.zerocopy;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+
 /**
+ * 服务器端
  * @author xieweidu
  * @createDate 2019-12-02 20:35
  */
 public class NewIOServer {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        InetSocketAddress inetSocketAddress = new InetSocketAddress(7001);
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        ServerSocket serverSocket = serverSocketChannel.socket();
+        serverSocket.bind(inetSocketAddress);
+        ByteBuffer byteBuffer = ByteBuffer.allocate(4096);
+        while (true){
+            SocketChannel socketChannel = serverSocketChannel.accept();
+            int readCount = 0;
+            while (-1!=readCount){
+                readCount = socketChannel.read(byteBuffer);
+            }
+            // byteBuffer倒带，position=0，mark作废
+            byteBuffer.rewind();
+        }
     }
 
 }
