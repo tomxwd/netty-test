f8e8a942cc4e01223695c8e00df6e2638c612d0e
tomxwd <648913416@qq.com>
2019-12-14 23:26:13 +0800

    nettyServer入门案例——客户端

---
+++ src/main/java/top/tomxwd/netty/simple/NettyClientHandler.java
@@ -0,0 +1,46 @@
+package top.tomxwd.netty.simple;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+/**
+ * @author xieweidu
+ * @createDate 2019-12-14 19:54
+ */
+public class NettyClientHandler extends ChannelInboundHandlerAdapter {
+    /**
+     * 当通道就绪的时候就触发该方法
+     *
+     * @param ctx
+     * @throws Exception
+     */
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("client :" + ctx);
+        ctx.writeAndFlush(Unpooled.copiedBuffer("hello server！！！", CharsetUtil.UTF_8));
+    }
+
+    /**
+     * 当通道有读取事件的时候，会触发该方法
+     *
+     * @param ctx
+     * @param msg
+     * @throws Exception
+     */
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = (ByteBuf) msg;
+        System.out.println("服务器回复的消息：" + buf.toString(CharsetUtil.UTF_8));
+        System.out.println("服务器的地址："+ctx.channel().remoteAddress());
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        System.out.println("客户端异常发生");
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
